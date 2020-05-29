691865faedbe5becd3ec86aa47546239147dbb4e
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-05-29 17:10:08 +0100

    +++

--- motamadris/test.py
+++ motamadris/test.py
@@ -2,24 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import json
 s=input("::")
-data=json.loads(open("%s.json"%(s)).read())
-for j in range(len(data)):
-	URL = data[j]["href"]
-	print(j)
-	try:
-		page = requests.get(URL)
-		soup = BeautifulSoup(page.content, 'html.parser')
-		R=[]
-		i=1
-		soup=soup.find("div",class_="entry-content")
-		for li in soup.findAll('li',class_='column'):
-			r=li.find("a")
-			h,t=r["href"],r.get_text()
-			R.append({"href":h,"title":t,"id":str(i)})
-			i+=1
-		open("%s_%d.json"%(s,j+1),"w").write(json.dumps(R))
-	except:pass
-	try:
+ddata=json.loads(open("%s.json"%(s)).read())
+for k in range(len(ddata)):
+	data=json.loads(open("%s_%d.json"%(s,k+1)).read())
+	for j in range(len(data)):
+		URL = data[j]["href"]
+		print(k,j)
+		# try:
+		# 	page = requests.get(URL)
+		# 	soup = BeautifulSoup(page.content, 'html.parser')
+		# 	R=[]
+		# 	i=1
+		# 	soup=soup.find("div",class_="entry-content")
+		# 	for li in soup.findAll('li',class_='column'):
+		# 		r=li.find("a")
+		# 		h,t=r["href"],r.get_text()
+		# 		R.append({"href":h,"title":t,"id":str(i)})
+		# 		i+=1
+		# 	open("%s_%d_%d.json"%(s,k+1,j+1),"w").write(json.dumps(R))
+		# except:pass
+		# try:
 		page = requests.get(URL)
 		soup = BeautifulSoup(page.content, 'html.parser')
 		soup=soup.findAll("div",class_="table-responsive")
@@ -30,7 +32,6 @@ for j in range(len(data)):
 				try:R.append({"href":r.find("a")["href"] ,"title":r.find("td").getText()})
 				except:pass
 				i+=1
-
-		open("P%s_%d.json"%(s,j+1),"w").write(json.dumps(R))
-	except:
-		print("error")
+		open("P%s_%d_%d.json"%(s,k+1,j+1),"w").write(json.dumps(R))
+		# except:
+		# 	print("error")	

0ef55fc1210907d4c2ec010ac1f5a9323e5fccf4
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-06-01 15:52:45 +0100

    ++++

--- test4.py
+++ test4.py
@@ -17,7 +17,22 @@ for i in r:
 			"title":t,
 			"href":h
 			})
+URL="http://exo7.emath.fr/"
+page = requests.get("http://exo7.emath.fr/prof.html")
+soup = BeautifulSoup(page.content, 'html.parser')
+r=soup.findAll('div',class_="fictitre")
 
+for i in r:
+	rr=i.find("a")
+	href=rr["href"]
+	if "pdf" in href:
+		t=rr.getText()
+		h=URL+rr["href"]
+		R.append({
+			"title":t,
+			"href":h
+			})
 a=open("P16.json","w")
-a.write(str(R))
+import json
+a.write(json.dumps(R))
 a.close()

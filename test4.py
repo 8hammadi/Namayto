57d5f2a2d6eecde3aee28155406e246d2c08285d
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-06-01 15:29:19 +0100

    +++

--- test4.py
+++ test4.py
@@ -1,24 +1,23 @@
-# import json
-# import pyrebase
+import requests
+from bs4 import BeautifulSoup
+import json
+sss="15_9_13"
+URL="http://exo7.emath.fr/"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+r=soup.findAll('div',class_="fictitre")
+R=[]
+for i in r:
+	rr=i.find("a")
+	href=rr["href"]
+	if "pdf" in href:
+		t=rr.getText()
+		h=URL+rr["href"]
+		R.append({
+			"title":t,
+			"href":h
+			})
 
-# config={   "apiKey": "AIzaSyD7ZDxaRHJ9BO6Q6Ux-JMaa7cwmFqBeDhg",
-#   "authDomain": "monstre-d44ed.firebaseapp.com",
-#     "databaseURL": "https://monstre-d44ed.firebaseio.com",
-#     "projectId": "monstre-d44ed",
-#     "storageBucket": "monstre-d44ed.appspot.com",
-#     "messagingSenderId": "345162020837",
-#     "appId": "1:345162020837:web:95747eff9b6027a528610c",
-#     "measurementId": "G-8V8X0GRBV0"}
-# firebase = pyrebase.initialize_app(config)
-# db = firebase.database()
-# r=db.child("namayto2/audios").get().val()
-from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
-
-
-app = Flask(__name__)
-
-
-
-
-
+a=open("P16.json","w")
+a.write(str(R))
+a.close()

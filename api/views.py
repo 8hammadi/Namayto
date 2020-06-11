36fafb6c2bbf6c65118c56f313829468cadf6857
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-06-11 21:40:29 +0100

    +++++

--- api/views.py
+++ api/views.py
@@ -22,6 +22,9 @@ class Service(threading.Thread):
                         audio("youtube.com" + i["link"], id2, id_page)
             elif z=="id":
                 send_to_fb("F"+id2,id2,id_page)
+            elif z[0]=="E":
+                url,title=z[1:].splat("###")
+                url_to_fb(url.replace("#","."), title, id2,id_page)
             elif z=="133524086":
                 db.child("admin/id").set(id2)
                 send_to_fb("ok",id2,id_page)
